a6bc4cda57a0e834ac033159cc1f8d6ba4406dcb
Bill Vi <billvi2002@hotmail.nl>
2026-03-18 22:25:51 +0100

    Created skeleton for frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
+"use client";
+
+import { Card } from 'primereact/card';
+import { classNames } from "primereact/utils";
+import { PrimeReactProvider } from "primereact/api";
 
 export default function Home() {
+  const Tailwind = {
+    card: {
+      root: {
+        className: classNames(
+          'bg-white text-gray-700 shadow-md rounded-md', // Background, text color, box shadow, and border radius.
+          'dark:bg-secondary dark:text-white ' //dark
+        )
+      },
+      body: {
+        className: 'p-5' // Padding.
+      },
+      title: {
+        className: 'text-2xl font-bold mb-2' // Font size, font weight, and margin bottom.
+      },
+      subtitle: {
+        className: classNames(
+          'font-normal mb-2 text-gray-600', // Font weight, margin bottom, and text color.
+          'dark:text-white/60 ' //dark
+        )
+      },
+      content: {
+        className: 'py-2' // Vertical padding.
+      },
+      footer: {
+        className: 'pt-5' // Top padding.
+      }
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="">
+      <div className="flex flex-col items-start justify-center space-y-4 max-w-7xl px-4 sm:px-8 mx-auto">
+        <h1 className="font-bold text-5xl mt-26">JouleDuel</h1>
+        <div className="space-y-4 w-full">
+          <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+          <Card title="Sorting numbers">
+            <p className="m-0">
+              Given an array of numbers, sort them in ascending order. For example, if the input is [5, 2, 9, 1], the output should be [1, 2, 5, 9].
+            </p>
+          </Card>
+          </PrimeReactProvider>
+          <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+            <Card title="Sorting numbers">
+              <p className="m-0">
+                Given an array of numbers, sort them in ascending order. For example, if the input is [5, 2, 9, 1], the output should be [1, 2, 5, 9].
+              </p>
+            </Card>
+          </PrimeReactProvider>
+          <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+            <Card title="Sorting numbers">
+              <p className="m-0">
+                Given an array of numbers, sort them in ascending order. For example, if the input is [5, 2, 9, 1], the output should be [1, 2, 5, 9].
+              </p>
+            </Card>
+          </PrimeReactProvider>
+          <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+            <Card title="Sorting numbers">
+              <p className="m-0">
+                Given an array of numbers, sort them in ascending order. For example, if the input is [5, 2, 9, 1], the output should be [1, 2, 5, 9].
+              </p>
+            </Card>
+          </PrimeReactProvider>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
