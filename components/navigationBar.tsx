d00c6a701bdfb6a7785db3398f1ede77ec10b235
Alessandro Valmori <a.valmori@student.tudelft.nl>
2026-03-25 14:05:12 +0100

    logo in navbar and favicon

--- components/navigationBar.tsx
+++ components/navigationBar.tsx
@@ -1,34 +1,43 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavigationBar() {
   return (
     <div className="fixed w-full top-0 z-50 dark:bg-secondary shadow">
       <div className="py-2 px-8 mx-auto w-full flex items-center justify-between relative max-w-7xl">
-        <div
-          className="font-bold text-4xl whitespace-nowrap w-full py-2 rounded-sm text-start">
-          <h1>JouleDuel</h1>
+        <div className="font-bold text-4xl whitespace-nowrap w-full py-2 rounded-sm text-start">
+          <h1 className="flex items-center gap-2">
+            JouleDuel
+            <Image
+              src="/JD_logo.png"
+              alt="JouleDuel logo"
+              width={48}
+              height={48}
+              className="h-12 w-12 bg-transparent"
+            />
+          </h1>
         </div>
         <nav className="flex items-center flex-1 justify-center">
           <ul className="flex items-center text-surface-400 select-none flex-row space-x-8 py-4 font-medium whitespace-nowrap">
             <li>
-                <Link
-                  href="/"
-                  className="flex items-center">
-                  <span className="dark:hover:text-gray-400 font-semibold">Problems</span>
-                </Link>
+              <Link href="/" className="flex items-center">
+                <span className="dark:hover:text-gray-400 font-semibold">
+                  Problems
+                </span>
+              </Link>
             </li>
             <li>
-                <Link
-                  href="/leaderboard"
-                  className="flex items-center">
-                  <span className="dark:hover:text-gray-400 font-semibold">Leaderboard</span>
-                </Link>
+              <Link href="/leaderboard" className="flex items-center">
+                <span className="dark:hover:text-gray-400 font-semibold">
+                  Leaderboard
+                </span>
+              </Link>
             </li>
             <li>
-              <Link
-                href="/code-help"
-                className="flex items-center">
-                <span className="dark:hover:text-gray-400 font-semibold">Code help</span>
+              <Link href="/code-help" className="flex items-center">
+                <span className="dark:hover:text-gray-400 font-semibold">
+                  Code help
+                </span>
               </Link>
             </li>
           </ul>
